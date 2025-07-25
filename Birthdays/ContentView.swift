2d1f397ee5f5c8a8d7ce75b25bbebe167cea1c48
Vasundhara Jagadeesan <vasundhara.jagadeesan@gmail.com>
2025-07-25 10:22:45 -0400

    Birthday app with feature to add new birthday name, and base names already present

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var friends: [Friend] = [ Friend(name: "Taylor Swift", birthday: .now),
+        Friend(name: "Frank Ocean", birthday: Date(timeIntervalSince1970: 0))
+    ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack{
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20){
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date){
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        //Resetting name and birthday to inital state
+                        newName = ""
+                        newBirthday = .now
+                    }
+                    .bold()
+                }
+                .padding()
+                .background(.bar)
+                
+            }
         }
-        .padding()
+        
     }
 }
 
