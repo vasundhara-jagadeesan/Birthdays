f3e74e9db18976fb0794de16c4d530eddc1b7324
Vasundhara Jagadeesan <vasundhara.jagadeesan@gmail.com>
2025-07-25 11:14:34 -0400

    Added delete function

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -19,12 +19,15 @@ struct ContentView: View {
     var body: some View {
         
         NavigationStack {
-            List(friends) { friend in
-                HStack{
-                    Text(friend.name)
-                    Spacer()
-                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+            List {
+                ForEach(friends) {friend in
+                    HStack{
+                        Text(friend.name)
+                        Spacer()
+                        Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                    }
                 }
+                .onDelete(perform: deleteFriend)
             }
             .navigationTitle("Birthdays")
             .safeAreaInset(edge: .bottom) {
@@ -51,6 +54,14 @@ struct ContentView: View {
         }
         
     }
+    
+    func deleteFriend(at offsets: IndexSet) {
+        for index in offsets {
+            let friendToDelete = friends[index]
+            context.delete(friendToDelete)
+        }
+    }
+    
 }
 
 #Preview {
