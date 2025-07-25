56230a4f69f740eb76228ccea037d8b657c6ee68
Vasundhara Jagadeesan <vasundhara.jagadeesan@gmail.com>
2025-07-25 10:59:31 -0400

    Added Swift Data so data on app can be saved now. Same name can have diff b'days.

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -6,19 +6,20 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     
-    @State private var friends: [Friend] = [ Friend(name: "Taylor Swift", birthday: .now),
-        Friend(name: "Frank Ocean", birthday: Date(timeIntervalSince1970: 0))
-    ]
+    @Query private var friends: [Friend]
+    @Environment(\.modelContext) private var context
+    
     @State private var newName = ""
     @State private var newBirthday = Date.now
     
     var body: some View {
         
         NavigationStack {
-            List(friends, id: \.name) { friend in
+            List(friends) { friend in
                 HStack{
                     Text(friend.name)
                     Spacer()
@@ -36,7 +37,7 @@ struct ContentView: View {
                     }
                     Button("Save") {
                         let newFriend = Friend(name: newName, birthday: newBirthday)
-                        friends.append(newFriend)
+                        context.insert(newFriend)
                         //Resetting name and birthday to inital state
                         newName = ""
                         newBirthday = .now
@@ -54,4 +55,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+        .modelContainer(for: Friend.self, inMemory: true)
 }
